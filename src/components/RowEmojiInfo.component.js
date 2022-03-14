7c745245c58c7e8543db0e03b2bb71e42388911f
kakavand <kakavandah@gmail.com>
2022-03-15 01:55:07 +0330

    add search

--- src/components/RowEmojiInfo.component.js
+++ src/components/RowEmojiInfo.component.js
@@ -2,8 +2,14 @@ import React from "react";
 import style from './component.module.scss'
 
 function RowEmojiInfo(props) {
+
+    const copyEmoji = (e) => {
+        navigator.clipboard.writeText(e.currentTarget.children[0].innerText)
+        alert(`ایموجی ${e.currentTarget.children[0].innerText} کپی شد`)
+    }
+
     return (
-        <div className={style.row}>
+        <div className={style.row} onClick={copyEmoji}>
             <h1>{props.character}</h1>
             <h1>{props.name}</h1>
         </div>
--- src/pages/Home/Home.page.js
+++ src/pages/Home/Home.page.js
@@ -4,15 +4,30 @@ import RowEmojiInfo from "../../components/RowEmojiInfo.component";
 
 function Home() {
     const [emojis, setEmojis] = useState([]);
+    const [all, setAll] = useState([]);
+    const [value, setfirst] = useState("");
 
     useEffect(() => {
         getEmoji().then((res) => setEmojis(res));
     }, []);
 
+    useEffect(() => {
+        setAll(emojis)
+    }, [emojis]);
+
+    const changeValu = ({ target }) => {
+        const ArrNew = emojis.filter((item) =>
+            item.unicodeName.includes(target.value)
+        );
+        console.log(ArrNew);
+        setAll(ArrNew);
+    };
+
     console.log(emojis.length);
     return (
         <>
-            {emojis.map((item) => (
+            <input type="text" onChange={changeValu} />
+            {all.map((item) => (
                 <RowEmojiInfo
                     character={item.character}
                     name={item.unicodeName}
